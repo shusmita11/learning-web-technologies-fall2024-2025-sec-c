5cb2842868609c245202e20fd66d0c79bf2409e1
shusmita11 <121575441+shusmita11@users.noreply.github.com>
2024-11-21 17:01:07 +0600

    Add files via upload

--- lab4/form5.php
+++ lab4/form5.php
@@ -7,31 +7,35 @@
         <fieldset>
             <legend>Degree</legend>
             <h3>
-                <input type="checkbox" name="degrees[]" value="SSC" > SSC
-                
+                <input type="checkbox" name="degrees[]" value="SSC"> SSC
                 <input type="checkbox" name="degrees[]" value="HSC"> HSC
-                
                 <input type="checkbox" name="degrees[]" value="BSc"> BSc
-
-            <input type="checkbox" name="degrees[]" value="MSc"> MSc
+                <input type="checkbox" name="degrees[]" value="MSc"> MSc
             </h3>
             <input type="submit" name="submit" value="Submit">
         </fieldset>
     </form>
 
     <?php
-    if (isset($_POST['degress[]']))
+    if(isset($_POST['degrees']))
     {
-       if(count(degress) >= 2)
-       {
+        $degrees = $_POST['degrees'];
+        if (count($degrees) >= 2)
+        {
+            echo "Valid selection";
+        }
         
-       }
+        else
+        {
+            echo "Invalid selection";
+        }
     }
     
-    else
+    else if(isset($_POST['submit']))
     {
-        echo "<p>No degrees selected</p>";
+        echo "Invalid selection";
     }
     ?>
+    
 </body>
 </html>
