3714d1dc0703114aa181cd8635d3029d562a3c60
shusmita11 <121575441+shusmita11@users.noreply.github.com>
2024-11-24 15:56:40 +0600

    Add files via upload

--- lab exam/home.php
+++ lab exam/home.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     if(!isset($_SESSION['status']))
     {
         header('location: login.html'); 
@@ -11,6 +12,15 @@
         </head>
         <body>
                 <h1>Welcome to Home</h1>
+                <?php
+                    //session_start();
+                    echo $_SESSION['user_info']['user'];
+
+                    foreach($_SESSION['user_info'] as $user)
+                    {
+                        echo $user.'<br>';
+                    }
+                ?>
                 <input type="submit" name="logout" value="Logout">
         </body>
     </html>
